4e5915423c233bfdb10a6b01285611b7c6f13616
guoxiujuan <836832555@qq.com>
2019-04-29 10:35:04 +0800

    手机页面

---
+++ www/js/lib.js
@@ -0,0 +1,14 @@
+﻿
+
+$(document).ready(function($) {
+	$(window).scroll(function(){
+		var _myHd = $(window).scrollTop();
+		if( _myHd<50 ){
+			$('#hd').removeClass('hdbg');
+		}else{
+			$('#hd').addClass('hdbg');
+		}
+	});
+
+
+});
